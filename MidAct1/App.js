7e1ddecbd53ef15e4492ab4f11ec0ec11989e960
Roslyn Contrevida <roslyncontrevida@gmail.com>
2025-09-14 19:16:20 +0800

    Added Activity5 and MidAct1 folders

---
+++ MidAct1/App.js
@@ -0,0 +1,12 @@
+import Comment from './Comment';
+import Messenger from './Messenger';
+import React from 'react';
+
+export default function App() {
+  return (
+    <>
+      <Messenger/>
+      <Comment/>
+    </>
+  )
+}
